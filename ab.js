37033b3d7fb9b17a2a2e4eb4b62b7e418c1bfe5c
StarSanju21 <m99717343@gmail.com>
2021-10-20 19:32:54 +0530

    first commit

---
+++ ab.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxB47a4j_X-Ee43lQSIJcyOKIDCAMqDkw",
+  authDomain: "mom-srecipes.firebaseapp.com",
+  databaseURL: "https://mom-srecipes-default-rtdb.firebaseio.com",
+  projectId: "mom-srecipes",
+  storageBucket: "mom-srecipes.appspot.com",
+  messagingSenderId: "63858305959",
+  appId: "1:63858305959:web:86d303447a9e1db7aca482"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
